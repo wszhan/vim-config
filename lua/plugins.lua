00994c9a12cb6cad1739b84a631d556af38bac10
wszhan <wszhan@@users.noreply.github.com>
2024-02-11 22:54:51 -0500

    format with LSP

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,42 +1,43 @@
 local plugins = {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
   {
     "ellisonleao/gruvbox.nvim",
-    priority = 1000, 
+    priority = 1000,
     config = true,
   },
 
   -- -- LSP Setup Start -- --
   --- Use the two plugins below if you want to manage the language servers from neovim
-  {'williamboman/mason.nvim'}, -- check README for requirements
-  {'williamboman/mason-lspconfig.nvim'},
+  { 'williamboman/mason.nvim' }, -- check README for requirements
+  { 'williamboman/mason-lspconfig.nvim' },
   -- dependencies for LSP Zero to work
   {
-    'VonHeikemen/lsp-zero.nvim', 
-    branch = 'v3.x', 
-    lazy = true, 
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
     config = false
   },
   {
     'neovim/nvim-lspconfig',
     dependencies = {
-      {'hrsh7th/cmp-nvim-lsp'},
+      { 'hrsh7th/cmp-nvim-lsp' },
     }
   },
   -- Autocompletion
   {
     'hrsh7th/nvim-cmp',
     dependencies = {
-      {'L3MON4D3/LuaSnip'}
+      { 'L3MON4D3/LuaSnip' }
     },
   },
   -- -- LSP Setup End -- --
 
   -- Pyright extension for neovim --
-  {'fannheyward/coc-pyright'},
+  { 'fannheyward/coc-pyright' },
 }
 
 return plugins
