016fbc8ab3b3f9835f7bfb7819f3227f961baed0
wszhan <wszhan@@users.noreply.github.com>
2025-04-11 18:32:23 -0400

    add ruff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,69 +1,54 @@
 local plugins = {
 
-	-- -- LSP Setup Start -- --
-	--- Use the two plugins below if you want to manage the language servers from neovim
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	}, -- check README for requirements
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"clangd",
-                    "cmake",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig", -- https://github.com/neovim/nvim-lspconfig
-		lazy = false,
-		config = function()
-			-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  -- -- LSP Setup Start -- --
+  --- Use the two plugins below if you want to manage the language servers from neovim
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  }, -- check README for requirements
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "clangd",
+          "cmake",
+          "ruff", -- install ruff through pip or brew
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig", -- https://github.com/neovim/nvim-lspconfig
+    lazy = false,
+    config = function()
+      -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-                -- cmd = {'clangd', '--log=verbose', '--pretty'}, -- Use LspLog to inspect debug log
-			})
-			lspconfig.cmake.setup({
-				capabilities = capabilities,
-			})
-		end,
-	},
-	-- dependencies for LSP Zero to work
-	--- {
-	---   'VonHeikemen/lsp-zero.nvim',
-	---   branch = 'v3.x',
-	---   lazy = true,
-	---   config = false
-	--- },
-	--- {
-	---   'neovim/nvim-lspconfig',
-	---   dependencies = {
-	---     { 'hrsh7th/cmp-nvim-lsp' },
-	---   }
-	--- },
-	-- Autocompletion
-	-- {
-	--   'hrsh7th/nvim-cmp',
-	--   dependencies = {
-	--     { 'L3MON4D3/LuaSnip' }
-	--   },
-	-- },
-	-- -- LSP Setup End -- --
-
-	-- Pyright extension for neovim --
-	-- { 'fannheyward/coc-pyright' },
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+        -- cmd = {'clangd', '--log=verbose', '--pretty'}, -- Use LspLog to inspect debug log
+      })
+      lspconfig.cmake.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ruff.setup({
+        capabilities = capabilities,
+        cmd = { "/usr/local/bin/ruff", "server" },
+        filetypes = { "python" },
+        root_dir = require("lspconfig.util").find_git_ancestor,
+        on_attach = on_attach,
+      })
+    end,
+  },
 }
 
 return plugins
+
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,6 +8,28 @@ return {
 				null_ls.builtins.formatting.stylua,
 				-- null_ls.builtins.completion.spell,
 				-- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+				null_ls.builtins.formatting.clang_format,
+
+				-- python: it seems none-ls removed builtin support for ruff; leave it here for now
+				-- null_ls.builtins.formatting.ruff.with({
+				-- 	command = "ruff",
+				-- 	args = {
+				-- 		"format", -- use ruff's format engine
+				-- 		"--stdin-filename",
+				-- 		"$FILENAME",
+				-- 		"-",
+				-- 	},
+				-- }),
+				-- null_ls.builtins.diagnostics.ruff.with({
+				-- 	command = "ruff",
+				-- 	args = {
+				-- 		"--quiet", -- hide unncessary output
+				-- 		"--stdin-filename",
+				-- 		"$FILENAME", -- tell ruff the actual filename
+				-- 		"-",
+				-- 	}, -- read code from stdin since null-ls feeds the rile to ruff this way
+				-- }),
+				-- null_ls.builtins.formatting.black,
 			},
 		})
 
