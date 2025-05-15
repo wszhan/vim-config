679e73841f779a34815833085a6f7354a5e3ef1a
wszhan <wszhan@@users.noreply.github.com>
2025-05-14 22:04:54 -0400

    add comments for telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,14 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
+    tag = "*", -- latest stable release
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
       local keybindings_opts = { noremap = true, silent = true }
       vim.keymap.set("n", "<leader>ff", builtin.find_files, keybindings_opts)
-      vim.keymap.set("n", "<leader>gs", builtin.grep_string, keybindings_opts) -- dependency: ripgrep via Homebrew
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>gs", builtin.grep_string, keybindings_opts) -- grep word under cursor
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" }) -- dependency: ripgrep via Homebrew
     end,
   },
   {
