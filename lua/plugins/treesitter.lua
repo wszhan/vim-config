6af7685c286e2e3d621ca30959b15415116bf14f
wszhan <wszhan@@users.noreply.github.com>
2025-05-14 22:16:51 -0400

    add comments for treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,17 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
+	build = ":TSUpdate", -- update parsers to latest stable versions
+    version = "*",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			-- ensure_installed = { "c", "cpp", "python", "lua", "javascript", "html" },
-			auto_install = true,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
+			ensure_installed = { "c", "cpp", "python", "lua", "javascript", "html", "markdown", "markdown_inline"}, -- install during setup
+			auto_install = true, -- auto-install parsers for current opened file if not specified above
+			sync_install = false, -- async install; avoid blocking
+			highlight = { enable = true }, -- syntax highlighting
+			indent = { enable = true }, -- smart indentation
 		})
 	end,
 }
+
